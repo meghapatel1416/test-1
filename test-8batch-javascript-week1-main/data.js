933e8976dd6669d3851adb0f4d5982281e6be008
Your Name <you@example.com>
2023-10-09 08:30:57 +0530

    test 1

---
+++ test-8batch-javascript-week1-main/data.js
@@ -0,0 +1,207 @@
+const data= [
+    {
+      "id": 1,
+      "company": "Photosnap",
+      "logo": "./images/photosnap.svg",
+      "new": true,
+      "featured": true,
+      "position": "Senior Frontend Developer",
+      "role": "Frontend",
+      "level": "Senior",
+      "postedAt": "1d ago",
+      "contract": "Full Time",
+      "location": "USA Only",
+      "languages": ["HTML", "CSS", "JavaScript"],
+      "tools": []
+    },
+    {
+      "id": 2,
+      "company": "Manage",
+      "logo": "./images/manage.svg",
+      "new": true,
+      "featured": true,
+      "position": "Fullstack Developer",
+      "role": "Fullstack",
+      "level": "Midweight",
+      "postedAt": "1d ago",
+      "contract": "Part Time",
+      "location": "Remote",
+      "languages": ["Python"],
+      "tools": ["React"]
+    },
+    {
+      "id": 3,
+      "company": "Account",
+      "logo": "./images/account.svg",
+      "new": true,
+      "featured": false,
+      "position": "Junior Frontend Developer",
+      "role": "Frontend",
+      "level": "Junior",
+      "postedAt": "2d ago",
+      "contract": "Part Time",
+      "location": "USA Only",
+      "languages": ["JavaScript"],
+      "tools": ["React", "Sass"]
+    },
+    {
+      "id": 4,
+      "company": "MyHome",
+      "logo": "./images/myhome.svg",
+      "new": false,
+      "featured": false,
+      "position": "Junior Frontend Developer",
+      "role": "Frontend",
+      "level": "Junior",
+      "postedAt": "5d ago",
+      "contract": "Contract",
+      "location": "USA Only",
+      "languages": ["CSS", "JavaScript"],
+      "tools": []
+    },
+    {
+      "id": 5,
+      "company": "Loop Studios",
+      "logo": "./images/loop-studios.svg",
+      "new": false,
+      "featured": false,
+      "position": "Software Engineer",
+      "role": "Fullstack",
+      "level": "Midweight",
+      "postedAt": "1w ago",
+      "contract": "Full Time",
+      "location": "Worldwide",
+      "languages": ["JavaScript"],
+      "tools": ["Ruby", "Sass"]
+    },
+    {
+      "id": 6,
+      "company": "FaceIt",
+      "logo": "./images/faceit.svg",
+      "new": false,
+      "featured": false,
+      "position": "Junior Backend Developer",
+      "role": "Backend",
+      "level": "Junior",
+      "postedAt": "2w ago",
+      "contract": "Full Time",
+      "location": "UK Only",
+      "languages": ["Ruby"],
+      "tools": ["RoR"]
+    },
+    {
+      "id": 7,
+      "company": "Shortly",
+      "logo": "./images/shortly.svg",
+      "new": false,
+      "featured": false,
+      "position": "Junior Developer",
+      "role": "Frontend",
+      "level": "Junior",
+      "postedAt": "2w ago",
+      "contract": "Full Time",
+      "location": "Worldwide",
+      "languages": ["HTML", "JavaScript"],
+      "tools": ["Sass"]
+    },
+    {
+      "id": 8,
+      "company": "Insure",
+      "logo": "./images/insure.svg",
+      "new": false,
+      "featured": false,
+      "position": "Junior Frontend Developer",
+      "role": "Frontend",
+      "level": "Junior",
+      "postedAt": "2w ago",
+      "contract": "Full Time",
+      "location": "USA Only",
+      "languages": ["JavaScript"],
+      "tools": ["Vue", "Sass"]
+    },
+    {
+      "id": 9,
+      "company": "Eyecam Co.",
+      "logo": "./images/eyecam-co.svg",
+      "new": false,
+      "featured": false,
+      "position": "Full Stack Engineer",
+      "role": "Fullstack",
+      "level": "Midweight",
+      "postedAt": "3w ago",
+      "contract": "Full Time",
+      "location": "Worldwide",
+      "languages": ["JavaScript", "Python"],
+      "tools": ["Django"]
+    },
+    {
+      "id": 10,
+      "company": "The Air Filter Company",
+      "logo": "./images/the-air-filter-company.svg",
+      "new": false,
+      "featured": false,
+      "position": "Front-end Dev",
+      "role": "Frontend",
+      "level": "Junior",
+      "postedAt": "1mo ago",
+      "contract": "Part Time",
+      "location": "Worldwide",
+      "languages": ["JavaScript"],
+      "tools": ["React", "Sass"]
+    }
+  ]
+
+  const display = () => {
+    const display = data.map ((v) => {
+        return `  <div class="col-md-4">
+        <div class="card">
+  
+            <div class="card-header bg-dark d-sm-flex justify-content-sm-between align-items-sm-center">
+              <div class=" card-title">
+                <a href="#" style="color:white" class="display-inline-   block" onmouseover="this.style.color='#00e6ac'" onmouseout="this.style.color='#fff'">
+           <strong>Job Title</strong>
+                </a>
+              </div>
+            </div>
+            <div class="card-body bg-light">
+             <p>${v.company}</p>
+             <p>${v.role}</p>
+             <p>${v.languages}</p>
+             <p>${v.location}</p>
+             <p>${v.position}</p>
+            </div>
+           
+          </div>
+        
+      </div> `
+    })
+
+    document.getElementById("myProducts").innerHTML = display.join("");
+  }
+  display();
+
+  myFunction = ()=> {
+    
+    let searchTerm = document.getElementById("myFilter").value;
+    let myFunction = data.filter((v)=> {
+
+        return v.position.includes(searchTerm);
+    });
+
+    console.log(myFunction);
+    }
+display(myFunction);
+
+data.sort(function(a,b) {
+  if(a.length.company > b.length.company)
+  {
+    return 1;
+  }
+  else
+  {
+    return -1;
+  }
+
+  });
+  console.log(data);
+  
